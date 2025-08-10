4e1617947bfd94e57beff6436382ee0d63458f14
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-08-10 17:46:16 +0530

    feat: created MintResponse struct

--- backend/src/types/mint.rs
+++ backend/src/types/mint.rs
@@ -11,3 +11,26 @@ pub struct MintData{
     pub is_initialized : bool,
     pub freeze_authority : Option<String>
 }
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct MintResponse{
+    pub mint_address : String,
+    pub owner : String,
+    pub mint_authority : String,
+    pub supply : u64,
+    pub decimal : u8,
+    pub is_initialized : bool,
+    pub freeze_authority : Option<String>,
+    pub metadata : PartialMetadata
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct PartialMetadata{
+    pub name : Option<String>,
+    pub symbol : Option<String>,
+    pub uri : Option<String>,
+    pub seller_fee_basis_points : u16,
+    pub update_authority : Option<String>,
+    pub primary_sale_happened : bool,
+    pub is_mutable : bool,
+}
