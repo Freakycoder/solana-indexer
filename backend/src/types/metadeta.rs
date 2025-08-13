34cfaf9ce41d72b720cffe3bd0eced627b5c8670
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-08-13 12:23:16 +0530

    fix : replaced custom struct with imported struct

--- backend/src/types/metadeta.rs
+++ backend/src/types/metadeta.rs
@@ -1,6 +1,6 @@
-use serde::{Deserialize, Serialize};
+use mpl_token_metadata::types::TokenStandard;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone)]
 pub struct Metadata{
     pub mint_address : String,
     pub metadata_address : String,
@@ -9,16 +9,7 @@ pub struct Metadata{
     pub uri : String,
     pub seller_fee_basis_points : u16,
     pub update_authority : String,
+    pub token_standard : Option<TokenStandard>,
     pub primary_sale_happened : bool,
     pub is_mutable : bool,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum TokenStandard {
-    NonFungible,
-    FungibleAsset,
-    Fungible,
-    NonFungibleEdition,
-    ProgrammableNonFungible,
-    ProgrammableNonFungibleEdition,
 }
