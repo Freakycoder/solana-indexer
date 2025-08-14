0c32eec29062127729f1b91d6e6eef9b5bbbeb49
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-08-14 19:45:20 +0530

    fix : shifted nft strcut from mint.rs

--- backend/src/types/elasticsearch.rs
+++ backend/src/types/elasticsearch.rs
@@ -1,13 +1,19 @@
-use serde::{Serialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize)]
 pub struct SearchResult{
     pub mint_address : String,
-    pub collection_name : String,
+    pub nft_name : String,
     pub score : f64
 }
 
 #[derive(Debug, Serialize)]
 pub struct SearchResponse{
     pub results : Vec<SearchResult>
+}
+
+#[derive(Debug, Clone,Serialize, Deserialize)]
+pub struct NftDoc{
+    pub mint_address : String,
+    pub nft_name : String
 }
