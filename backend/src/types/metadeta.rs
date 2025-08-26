b43f60682e2787353473657c1e8d617c172db48e
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-08-26 20:15:39 +0530

    fix: updated symbol type

--- backend/src/types/metadeta.rs
+++ backend/src/types/metadeta.rs
@@ -5,7 +5,7 @@ pub struct Metadata{
     pub mint_address : String,
     pub metadata_address : Option<String>,
     pub name : String,
-    pub symbol : String,
+    pub symbol : Option<String>,
     pub uri : String,
     pub seller_fee_basis_points : u16,
     pub update_authority : String,
