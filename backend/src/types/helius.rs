d27dd3a07b11e19395e5ecbc43a8c91177bc042c
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2025-08-26 13:00:06 +0530

    feat: added request body struct

--- backend/src/types/helius.rs
+++ backend/src/types/helius.rs
@@ -1,4 +1,12 @@
-use serde::{Deserialize};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize,Serialize)]
+pub struct RequestBody{
+    pub jsonrpc: String,
+    pub id: u64,
+    pub method: String,
+    pub params: serde_json::Value,
+}
 
 #[derive(Debug, Deserialize)]
 pub struct HeliusAssetResponse {
